ef79ff608236a7e666866d51505aab0d545bebab
Julia <juliabestetidantas@gmail.com>
2026-08-15 00:32:14 -0300

    Configuração Inicial de OpenSpec e Graphify

---
+++ src/main/java/com/projeto/gestaoacoes/GestaoacoesApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.gestaoacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoacoesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestaoacoesApplication.class, args);
+    }
+
+}
