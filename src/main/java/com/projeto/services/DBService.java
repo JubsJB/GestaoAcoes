d1aef56f8a82b9eadff54067828c69a9923e2ea9
Julia <juliabestetidantas@gmail.com>
2026-08-17 22:07:03 -0300

    Configurações básicas (package, properties, DevConfig, TestConfig, DBService)

---
+++ src/main/java/com/projeto/services/DBService.java
@@ -0,0 +1,9 @@
+package com.projeto.services;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class DBService {
+}
